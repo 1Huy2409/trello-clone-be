e08b7e7530f7250e97430b759760e93b4c3b841d
1Huy2409 <nhathuy2409@gmail.com>
2025-12-17 22:01:12 +0700

    feat: new redis config

--- src/config/redis.config.ts
+++ src/config/redis.config.ts
@@ -2,16 +2,27 @@ import Redis from "ioredis";
 import dotenv from "dotenv";
 
 dotenv.config();
-const redis = new Redis({
+const redisConfig = {
     host: process.env.REDIS_HOST || "localhost",
     port: Number(process.env.REDIS_PORT) || 6379,
-});
-
-redis.on("connect", () => {
+}
+export const redisCache = new Redis(redisConfig);
+redisCache.on("connect", () => {
     console.log("Connected to Redis server");
 });
 
-redis.on("error", (err) => {
+redisCache.on("error", (err) => {
     console.error("Redis connection error:", err);
 });
-export default redis;
+
+export const redisSubscriber = new Redis(redisConfig);
+export const redisPublisher = new Redis(redisConfig);
+[redisPublisher, redisSubscriber].forEach((client, index) => {
+    client.on('connect', () => {
+        console.log(`Redis client ${index} connected`);
+    });
+
+    client.on('error', (err) => {
+        console.error(`Redis client ${index} error`, err);
+    });
+});
